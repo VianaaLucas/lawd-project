83e559af876dc2d74c49fb6e1d8fccb04495e10c
lucas <lucas@DESKTOP-R00O9GP>
2018-03-05 17:34:00 -0300

    Preparação para receber pagamentos de diferentes formas juntas (Dinheiro, cartão de crédito e débito juntos) 	Criação dos atributos nas classes 	Estrutura da tabela do banco de dados foi atualizada para tal ação

--- src/java/Control/ControleCarrinho.java
+++ src/java/Control/ControleCarrinho.java
@@ -1,4 +1,4 @@
-package Control;
+ package Control;
 
 import Model.ItemDeCompra;
 import Model.DAO.PedidoDAO;
--- src/java/Model/Pedido.java
+++ src/java/Model/Pedido.java
@@ -6,32 +6,62 @@ import java.util.List;
 
 public class Pedido {
 
-	private int id;
+    private int id;
 
-	private double valor_total;
+    private double valor_total;
 
-	private Date data;
+    private double valor_dinheiro;
 
-	private List<ItemPedido> itensPedido;
+    private double valor_ccredito;
 
-	private ClienteJuridico cliente;
+    private double valor_cdebito;
 
-	private Funcionario funcionario;
+    private Date data;
 
-	public void add_item(ItemPedido item) {
+    private List<ItemPedido> itensPedido;
 
-	}
+    private ClienteJuridico cliente;
 
-	public void add_funcionario(Funcionario func) {
+    private Funcionario funcionario;
 
-	}
+    public double getValor_dinheiro() {
+        return valor_dinheiro;
+    }
 
-	public void add_cliente(ClienteJuridico cli) {
+    public void setValor_dinheiro(double valor_dinheiro) {
+        this.valor_dinheiro = valor_dinheiro;
+    }
 
-	}
+    public double getValor_ccredito() {
+        return valor_ccredito;
+    }
 
-	public void baixar_estoque() {
+    public void setValor_ccredito(double valor_ccredito) {
+        this.valor_ccredito = valor_ccredito;
+    }
 
-	}
+    public double getValor_cdebito() {
+        return valor_cdebito;
+    }
+
+    public void setValor_cdebito(double valor_cdebito) {
+        this.valor_cdebito = valor_cdebito;
+    }
+
+    public void add_item(ItemPedido item) {
+
+    }
+
+    public void add_funcionario(Funcionario func) {
+
+    }
+
+    public void add_cliente(ClienteJuridico cli) {
+
+    }
+
+    public void baixar_estoque() {
+
+    }
 
 }
