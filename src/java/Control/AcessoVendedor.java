7d4263d5c7c17318b023b912f13cf16caaea5e11
lucas <lucas@DESKTOP-R00O9GP>
2018-04-04 17:11:11 -0300

    Pagamento com pagamento particionado esta funcionando 	Precisa de ajustes no front 		DIV com tabela fica embaixo da DIV de pagamentos Fechamento de pedido está OK 	Falta trava nos botões

---
+++ src/java/Control/AcessoVendedor.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Control;
+
+import Model.Usuario;
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author lucas
+ */
+public class AcessoVendedor implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpSession sessaoUsuario = ((HttpServletRequest) request).getSession();
+        Usuario usuarioLogado = (Usuario) sessaoUsuario.getAttribute("usuarioAutenticado");
+
+        if (usuarioLogado != null) {
+            chain.doFilter(request, response);
+        } else {
+            ((HttpServletResponse) response).sendRedirect("login.jsp");
+        }
+    }
+
+    @Override
+    public void destroy() {
+        
+    }
+    
+}
